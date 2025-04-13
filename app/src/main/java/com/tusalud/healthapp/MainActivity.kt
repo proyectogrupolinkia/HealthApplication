38dd571d70e489dcc3cbb90679bdd165527d2205
Javi <javierperezplanas@hotmail.com>
2025-04-13 09:00:44 +0200

    iniciamos crahslitings

--- app/src/main/java/com/tusalud/healthapp/MainActivity.kt
+++ app/src/main/java/com/tusalud/healthapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tusalud.healthapp.ui.theme.HealthappTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +36,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.clickable{
+            throw RuntimeException("iniciamos Crashlitics")
+        }
     )
 }
 
